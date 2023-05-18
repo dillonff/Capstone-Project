2f6f5734ad6e90d6507f4a786b7860f37dcb0340
Merrittwyz <ywan0029@uni.sydney.edu.au>
2023-05-18 19:23:59 +1000

    file upload

--- chat_server/src/main/java/au/edu/sydney/comp5703/cs30/chat/controller/FileController.java
+++ chat_server/src/main/java/au/edu/sydney/comp5703/cs30/chat/controller/FileController.java
@@ -1,11 +1,94 @@
 package au.edu.sydney.comp5703.cs30.chat.controller;
 
+import au.edu.sydney.comp5703.cs30.chat.entity.File;
+import au.edu.sydney.comp5703.cs30.chat.mapper.FileMapper;
+import au.edu.sydney.comp5703.cs30.chat.payload.UploadFileResponse;
 import au.edu.sydney.comp5703.cs30.chat.service.FileStorageService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileStorageService fileStorageService;
+    @Autowired
+    private FileMapper fileMapper;
+
+    @PostMapping("/uploadFile")
+    public File uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        File uploadFile = new File(fileName, fileDownloadUri, file.getSize());
+        fileMapper.insertFile(uploadFile);
+
+        return uploadFile;
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<File> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
+
+
+
+
+
+/***
 public class FileController {
 
     private FileStorageService storageService;
@@ -19,3 +102,4 @@ public class FileController {
         return "";  // TODO: returns  file id
     }
 }
+ ***/
---
+++ chat_server/src/main/java/au/edu/sydney/comp5703/cs30/chat/service/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package au.edu.sydney.comp5703.cs30.chat.service.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
